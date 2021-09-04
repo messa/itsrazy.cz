5f6322f97ac226d49c2b1057f8e61b3288b21f26
Petr Messner <petr.messner@gmail.com>
2021-09-04 11:31:05 +0200

    trying to load YAML files in getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,40 @@
-function IndexPage() {
+import fs from 'fs'
+import { resolve } from 'path'
+import yaml from 'js-yaml'
+
+function IndexPage({ foo }) {
   return (
     <div>
       <h1>itsrazy.cz</h1>
+      <pre>{JSON.stringify(foo, null, 2)}</pre>
     </div>
   )
+}
+
+export async function getStaticProps(context) {
+  const dataDir = findDataDir(__dirname)
+  const allData = []
+  fs.readdirSync(dataDir).forEach(file => {
+    if (file.endsWith('.yaml')) {
+      const filePath = dataDir + '/' + file
+      const fileContents = fs.readFileSync(filePath, 'utf8')
+      const fileData = yaml.load(fileContents)
+      allData.push({ filePath, fileData })
+    }
+  })
+  return {
+    props: { // will be passed to the page component as props
+      foo: allData,
+    },
+  }
+}
+
+function findDataDir(path) {
+  const dataDir = resolve(path + '/data')
+  if (fs.existsSync(dataDir) && fs.lstatSync(dataDir).isDirectory()) {
+    return dataDir
+  }
+  return findDataDir(path + '/..')
 }
 
 export default IndexPage
