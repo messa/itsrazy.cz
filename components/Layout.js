cedf30c0127e4f709ff0b828f9889ebd398812b6
Petr Messner <petr.messner@gmail.com>
2021-09-07 00:43:59 +0200

    add Head + title

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,13 @@
+import Head from 'next/head'
 import styles from './Layout.module.css'
 
 function Layout({ children }) {
-  return <div className={styles.container}>{children}</div>
+  return <div className={styles.container}>
+    <Head>
+      <title>ITsrazy.cz</title>
+    </Head>
+    {children}
+  </div>
 }
 
 export default Layout
